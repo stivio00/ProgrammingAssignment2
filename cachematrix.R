792caf8a0c7522473995646995e494dad9e99278
stivio00 <krol17@gmail.com>
2020-07-09 12:32:25 -0500

    First try

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
+## Assg 2 by Stephen Krol
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function cache the matrix inverse in m hided variable
+## m is only available troug get function
+## and redirects mean to solve
+makeCacheMatrix <- function(x) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+}
+##
+## Same here, changed "mean" to "solve" and "m" to "s"
+cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  
+  if(!is.null(m)) {
+    message("Resolviendo matriz inversa (English: Doing inverse)")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Will run a test with a matrix nXn
+test <- function(n=4){
+  x = matrix(sample(1:50, n*n), n, n)
+  
+  matrix = makeCacheMatrix(x)
+  
+  cacheSolve(matrix)
 }
